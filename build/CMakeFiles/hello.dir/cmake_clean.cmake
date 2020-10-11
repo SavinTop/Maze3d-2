e4adff7e51b9f54d7a95e921b2c1d0b08183e359
Savin <savinov.vladislav@gmail.com>
2020-10-11 18:15:29 +0300

    Initial commit.

---
+++ build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/main.cpp.obj"
+  "CMakeFiles/hello.dir/src/glad.c.obj"
+  "hello.exe"
+  "hello.exe.manifest"
+  "hello.pdb"
+  "libhello.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
